8b03a84fa3d1014d6ea180e9b5b1ba750d2959b0
Fabian Pena <camilo.fabis@gmail.com>
2020-10-24 10:35:30 -0400

    changed tours for places

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar(props) {
     <div className="ui fluid six item huge borderless menu top fixed">
     <div class="header item">305 Mia Places</div>
       <Link className="item" to="/">Home</Link>
-      <Link className="item" to="/tours">Tours</Link>
+      <Link className="item" to="/tours">Places</Link>
       <Link className="item" to="/about">About Us</Link>
       <Link className="item" to="/contact">Contact Us</Link>
     </div>
